e2ab3f8118318cc250ce878c42b6b23abe696217
adityagurav40 <113020833+adityagurav40@users.noreply.github.com>
2023-04-11 13:00:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,19 +5,18 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function () {
-		console.log("Hello, my name is Alice, I am 25 years old.")		
+		console.log("Hello, my name is Alice, I am 25 years old.");		
 	};
 
 function Employee(name, age, jobTitle) {
-	Person.call(this,name,age);
-	this.jobTitle = jobTitle;
-	
+	Person.call(this , name , age);
+	this.jobTitle = jobTitle;	
 }
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function () {
-	Hello, my name is Bob, I am 30 years old, and my job title is Manger.	
+	console.log("Hello, my name is Bob, I am 30 years old, and my job title is Manger.");	
 }
 // Do not change code below this line
 window.Person = Person;
