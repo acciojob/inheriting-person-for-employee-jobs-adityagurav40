a28dafde7ad85f546494591741fe4b0c12490b3f
adityagurav40 <113020833+adityagurav40@users.noreply.github.com>
2023-04-11 13:05:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-// complete this js code
+// Person constructor function
 function Person(name, age) {
-	this.name = name;
-	this.age = age;
+  this.name = name;
+  this.age = age;
 }
 
-Person.prototype.greet = function () {
-		console.log("Hello, my name is Alice, I am 25 years old.");		
-	};
+// Greet method on Person prototype
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
 
+// Employee constructor function
 function Employee(name, age, jobTitle) {
-	Person.call(this , name , age);
-	this.jobTitle = jobTitle;	
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
 }
+
+// Inherit Person prototype in Employee prototype
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
-Employee.prototype.jobGreet = function () {
-	console.log("Hello, my name is Bob, I am 30 years old, and my job title is Manger.");	
-}
+// JobGreet method on Employee prototype
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
